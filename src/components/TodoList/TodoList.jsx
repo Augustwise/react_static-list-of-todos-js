034ce47ac361cde2faf23ca552eebace9265b182
Mykola <nyr3mvneov@gmail.com>
2025-09-22 22:23:29 +0300

    some fixes_2

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,11 +3,9 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 import '../../App.scss';
 
 export const TodoList = ({ todos }) => (
-  <>
+  <section className="TodoList">
     {todos.map(todo => (
-      <section className="TodoList">
-        <TodoInfo todo={todo} key={todo.id} />
-      </section>
+      <TodoInfo todo={todo} key={todo.id} />
     ))}
-  </>
+  </section>
 );
